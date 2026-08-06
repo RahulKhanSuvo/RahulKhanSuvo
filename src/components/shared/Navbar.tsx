b7a287c2a022832fdaa10f38ec79d88db09bb73e
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-07 03:12:14 +0600

    feat: add Navbar component

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Container } from "../common/Container";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full pt-5">
+      <Container className="flex justify-between items-center">
+        {/*logo*/}
+        <div className="flex flex-col font-bold font-heading text-[3rem] leading-none">
+          <span>RAHUL</span>
+          <span className="pl-5">KHAN</span>
+        </div>
+        {/*nav*/}
+        <div className="flex gap-4">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+        </div>
+      </Container>
+    </nav>
+  );
+};
+export default Navbar;
