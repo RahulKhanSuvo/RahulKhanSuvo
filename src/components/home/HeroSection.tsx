b6cb741d10e5229a5f6e69b49468e5fe304a9f44
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-07 03:12:10 +0600

    feat: add social links and status to hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { Container } from "../common/Container";
 import {
   ScrollVelocityContainer,
   ScrollVelocityRow,
@@ -5,9 +6,9 @@ import {
 
 function HeroSection() {
   return (
-    <section>
-      <div className="relative flex w-full flex-col items-center justify-center overflow-hidden h-screen">
-        <ScrollVelocityContainer className="font-bold font-heading first-letter:uppercase leading-none md:text-[15rem] pt-20">
+    <section className="relative overflow-hidden h-screen flex justify-between flex-col py-5">
+      <div className="relative">
+        <ScrollVelocityContainer className="font-bold font-heading first-letter:uppercase leading-none md:text-[16rem] pt-30">
           <ScrollVelocityRow
             baseVelocity={18}
             direction={1}
@@ -34,6 +35,20 @@ function HeroSection() {
           </ScrollVelocityRow>
         </ScrollVelocityContainer>
       </div>
+      <Container className="flex justify-between items-center gap-4 w-full">
+        {/*socil link*/}
+        <div className="flex gap-4">
+          <a href="#">Github</a>
+          <a href="#">LinkedIn</a>
+          <a href="#">WhatsApp</a>
+        </div>
+        {/*open to work*/}
+        <div className="flex items-center gap-2">
+          {/*blink dot*/}
+          <div className="w-2 h-2 font-light rounded-full animate-blink bg-green-500"></div>
+          <p>OPEN TO WORK</p>
+        </div>
+      </Container>
     </section>
   );
 }
