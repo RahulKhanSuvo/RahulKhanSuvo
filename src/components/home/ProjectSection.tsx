fcb0137955d4a140fb33de3d628b492540bad764
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-07 16:21:53 +0600

    feat: add ProjectSection component

---
+++ src/components/home/ProjectSection.tsx
@@ -0,0 +1,9 @@
+import { SectionTitle } from "../shared/SectionTitle";
+
+export const ProjectSection = () => {
+  return (
+    <section>
+      <SectionTitle>Featured Work</SectionTitle>
+    </section>
+  );
+};
