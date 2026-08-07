462f5b3be14f406edfbacfdb3f5bd22d198b1e14
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-07 19:22:14 +0600

    feat: add SectionTitle component

---
+++ src/components/shared/SectionTitle.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { HyperText } from "../ui/hyper-text";
+
+export const SectionTitle = ({
+  children,
+  className,
+}: {
+  children: string;
+  className?: string;
+}) => {
+  return (
+    <HyperText
+      className={cn(
+        "uppercase text-black text-[6rem] font-bold font-heading leading-none",
+        className,
+      )}
+    >
+      {children}
+    </HyperText>
+  );
+};
