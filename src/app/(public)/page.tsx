5e6eafcaad807d4dd56f7037a9a2fec1848464a8
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-07 19:21:22 +0600

    ui: add z-index to navbar for overlay support ui: add z-index to navbar and update sections
    - Enable AboutSection on the home page
    - Increase max-width for container lg variant

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,12 @@
 import HeroSection from "@/components/home/HeroSection";
-// import { AboutSection } from "@/components/home/AboutSection";
+import { AboutSection } from "@/components/home/AboutSection";
 import { ProjectSection } from "@/components/home/ProjectSection";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
-      {/*<AboutSection />*/}
+      <AboutSection />
       <ProjectSection />
     </div>
   );
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,7 +2,7 @@ import { Container } from "../common/Container";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full pt-5">
+    <nav className="fixed top-0 left-0 w-full pt-5 z-50">
       <Container className="flex justify-between items-center">
         {/*logo*/}
         <div className="flex flex-col font-bold font-heading text-[3rem] leading-none">
@@ -10,7 +10,7 @@ const Navbar = () => {
           <span className="pl-5">KHAN</span>
         </div>
         {/*nav*/}
-        <div className="flex gap-4">
+        <div className="flex gap-4 ">
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Services</a>
