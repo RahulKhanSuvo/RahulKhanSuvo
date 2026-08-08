e3f6eb1f93d6166be365a198fc7dfa8f5c6f0f81
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-08 14:13:06 +0600

    feat: add SlideShow component

---
+++ src/components/home/SlideShow.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Image, { StaticImageData } from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+import { cn } from "@/lib/utils";
+
+interface SlideShowProps {
+  images: {
+    src: StaticImageData | string;
+    alt: string;
+  }[];
+  className?: string;
+}
+
+function SlideShow({ images, className }: SlideShowProps) {
+  return (
+    <Swiper
+      modules={[Autoplay, EffectFade]}
+      effect="fade"
+      fadeEffect={{
+        crossFade: true,
+      }}
+      loop
+      speed={100}
+      autoplay={{
+        delay: 200,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: false,
+      }}
+      allowTouchMove={false}
+      className={cn("h-75 w-3/5", className)}
+    >
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <div className="relative h-full w-full">
+            <Image
+              src={image.src}
+              alt={image.alt}
+              fill
+              priority={index === 0}
+              className="object-cover"
+            />
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default SlideShow;
