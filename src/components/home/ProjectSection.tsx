b8957a1e527f913abe41e9271bac92c1ac8fdb77
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-08 14:12:38 +0600

    feat: render project cards in project section

--- src/components/home/ProjectSection.tsx
+++ src/components/home/ProjectSection.tsx
@@ -1,12 +1,19 @@
+import { projectData } from "@/data/projectData";
 import { Container } from "../common/Container";
 import { SectionTitle } from "../shared/SectionTitle";
+import { ProjectCard } from "./ProjectCard";
 
 export const ProjectSection = () => {
   return (
     <section>
       <Container>
-        <SectionTitle>Featured Work</SectionTitle>
+        <SectionTitle className="text-center">Featured Work</SectionTitle>
         {/**/}
+        <div className="mt-16 grid grid-cols-2 gap-5 place-items-center">
+          {projectData.map((project, index) => (
+            <ProjectCard key={index} {...project} />
+          ))}
+        </div>
       </Container>
     </section>
   );
