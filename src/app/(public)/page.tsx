8cf0c4c881b172e4aa9f465b2602a3e5c8a51c37
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-08 18:52:23 +0600

    feat: add tech stack and footer to home page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,9 @@
 import HeroSection from "@/components/home/HeroSection";
 import { AboutSection } from "@/components/home/AboutSection";
 import { ServicesSection } from "@/components/home/ServicesSection";
+import { TechStackSection } from "@/components/home/TechStackSection";
 import { ProjectSection } from "@/components/home/ProjectSection";
+import { Footer } from "@/components/shared/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +12,9 @@ export default function Home() {
       <AboutSection />
       <ProjectSection />
       <ServicesSection />
+      <TechStackSection />
+      <Footer />
     </div>
   );
 }
+
