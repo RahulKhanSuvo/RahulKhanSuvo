5e7d6f95398ca5fffc24057b032a667aad019d41
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-22 00:46:16 +0600

    User Safety: safe

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,5 +1,3 @@
-import { StaticImageData } from "next/image";
-
 import galleryImage1 from "@/assets/projects/1.webp";
 import galleryImage2 from "@/assets/projects/3.webp";
 import galleryImage3 from "@/assets/projects/4.webp";
@@ -13,36 +11,7 @@ import livoProductPage from "@/assets/projects/livo/livoProductPage.png";
 import livoDashboard from "@/assets/projects/livo/livoDashboard.png";
 import livoCatalog from "@/assets/projects/livo/livoDashbaordCatalog.png";
 import liveProductSider from "@/assets/projects/livo/livoSlider.png";
-
-interface CaseStudySection {
-  index: string;
-  title: string;
-  description: string;
-}
-type ProjectAspect = "hero" | "tall" | "wide";
-export interface Project {
-  id?: number;
-  title: string;
-  category: string;
-  year: string;
-  aspect: ProjectAspect;
-  description: string;
-  stack: string[];
-  live: string;
-  github: string;
-  isFeatured?: boolean;
-  image: StaticImageData;
-  gallery: {
-    src: StaticImageData;
-    alt: string;
-  }[];
-  caseStudy: {
-    intro: CaseStudySection;
-    challenge: CaseStudySection;
-    solution: { index: string; text: string; brand: string };
-    result: { index: string; heading: string };
-  };
-}
+import { Project } from "@/types/project.type";
 
 export const projectData: Project[] = [
   {
