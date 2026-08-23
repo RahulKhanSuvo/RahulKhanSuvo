d0903db588b489be798b6c4ce7322f0b05d25eb6
Rahul Khan suvo <rahul.khan.suvo@gmail.com>
2026-08-23 09:54:33 +0600

    ui: adjust hero section height and typography styles

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -8,12 +8,12 @@ import GlitchImage from "./GlitchImage";
 
 function HeroSection() {
   return (
-    <section className="relative overflow-hidden h-screen flex justify-between flex-col py-5 px-1">
+    <section className="relative overflow-hidden  md:h-[70vh] lg:h-screen flex justify-between flex-col py-5 px-1">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10 w-[62vw] max-w-72 sm:max-w-80 md:w-88 aspect-87.5/130">
         <GlitchImage src={heroImage} alt="Hero" />
       </div>
       <div className="relative">
-        <ScrollVelocityContainer className="font-bold font-heading first-letter:uppercase text-[12rem]/[0.9] md:text-[14rem]/[0.9] lg:text-[16rem]/[0.9] pt-18 sm:pt-24 md:pt-30">
+        <ScrollVelocityContainer className="font-bold font-heading text-[12rem]/[0.9] md:text-[14rem]/[0.9] lg:text-[16rem]/[0.9] pt-18 sm:pt-24 md:pt-30">
           <ScrollVelocityRow
             baseVelocity={18}
             direction={1}
